2781f42ef726cd2be9d066969fa470f291e87461
lhatch98 <lukehatcher98@gmail.com>
2020-01-22 14:16:13 -0800

    write dat file fxn

--- protonated_clusters/potential_evaluations.py
+++ protonated_clusters/potential_evaluations.py
@@ -1,18 +1,36 @@
 import numpy as np
 import subprocess as sub
 
-def write_file(numb_oo_shifts, numb_h_shifts, numb_atoms):
+
+def write_file(filename, filename_save, atoms_list):
+        oo_geoms = np.load(filename)
+        # numb_geoms = len(np.vstack(oo_geoms))
+        f = open(filename_save, "w")
+        f.write(str(len(atoms_list)) + "\n" + str(len(oo_geoms)) + "\n")
+        for i in range(len(oo_geoms)):
+            one_hyd_shift = oo_geoms[i]
+            element = 0
+            for line in one_hyd_shift:
+                f.write(f"{line[0]} {line[1]} {line[2]} {atoms_list[j]} \n")
+                element += 1
+        f.close()
+        return None
+
+
+atoms_list = ["H", "H", "H", "O", "H", "H", "O", "H", "H", "O"]
+run = write_file2("dvr_oo_geoms_shift_1.npy", "coord.dat", atoms_list)
+
+
+def get_dat_files(numb_oo_shifts, filename): #filename must end in _#
     for i in range(1, numb_oo_shifts + 1):
-        a = np.load("dvr_oo_geoms_shift_" + str(i) + ".npy")
-        a = np.reshape(a, ((numb_oo_shifts * numb_h_shifts * numb_atoms) + 10, 3))  # +10 for the start_arrays in dvr.py
-        file = open("data.dat", "w+")
-    return a, file
+        a = np.load(filenames + str(i) + ".npy")
+        b = write_file2()
+    return None
 
 
-run = write_file(10, 10, 10)
-# print(run)
 
 
 def get_pot():
     sub.run("./getpot, cwd="?")
-    return _
+    np.loadtxt(hlgjf)[:,0]
+    return
