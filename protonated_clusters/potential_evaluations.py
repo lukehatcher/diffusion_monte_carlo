7a84794ed6de8ab924c6c13c4472a931f51d1a59
lhatch98 <lukehatcher98@gmail.com>
2020-01-24 23:45:24 -0800

    get_pot fxn

--- protonated_clusters/potential_evaluations.py
+++ protonated_clusters/potential_evaluations.py
@@ -11,26 +11,30 @@ def write_file(filename, filename_save, atoms_list):
             one_hyd_shift = oo_geoms[i]
             element = 0
             for line in one_hyd_shift:
-                f.write(f"{line[0]} {line[1]} {line[2]} {atoms_list[j]} \n")
+                f.write(f"{line[0]} {line[1]} {line[2]} {atoms_list[element]} \n")
                 element += 1
         f.close()
         return None
 
 
 atoms_list = ["H", "H", "H", "O", "H", "H", "O", "H", "H", "O"]
-run = write_file2("dvr_oo_geoms_shift_1.npy", "coord.dat", atoms_list)
+# run = write_file2("dvr_oo_geoms_shift_1.npy", "coord.dat", atoms_list)
 
 
-def get_dat_files(numb_oo_shifts, filename): #filename must end in _#
+def get_dat_files_andpot(numb_oo_shifts):
     for i in range(1, numb_oo_shifts + 1):
-        a = np.load(filenames + str(i) + ".npy")
-        b = write_file2()
+        a = write_file("dvr_oo_geoms_shift_" +str(i) + ".npy", 'pot/exec/Files/coord.dat', atoms_list)
+        b = get_pot(i)
+
     return None
 
 
+def get_pot(lame_i):
+    sub.run("./getpot", cwd="pot/execFiles")
+    a = np.loadtxt("pot/execFiles/eng_dip.dat")[:, 0]
+    a *= 219474
+    np.savetxt(X=a, fname="pots_" + str(lame_i))
+    return None
 
 
-def get_pot():
-    sub.run("./getpot, cwd="?")
-    np.loadtxt(hlgjf)[:,0]
-    return
+run = get_dat_files_andpot(10)
